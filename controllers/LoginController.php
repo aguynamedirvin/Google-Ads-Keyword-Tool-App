7b3b7c4ec8e4cff8c2d13cca4f6a81c726aafe39
Irvin <irvindominguez1@gmail.com>
2023-11-21 15:37:35 -0700

    Added AJAX end points to signup and login

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -5,17 +5,47 @@
 require_once __DIR__ . '/../models/User.php';
 require_once __DIR__ . '/../models/Login.php';
 
+
 class LoginController {
+    private $authService;
+
+    public function __construct() {
+        $this->authService = new Login();
+        $this->user = new User();
+    }
+
     public function index() {
 
         $user = new User();
-        $login = new Login((new Database())->getConnection());
 
         // Check if user is logged in
         if ($user->isLoggedIn()) {
             header('Location: /dashboard');
         }
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $data = $this->getRequestData();
+            $response = $this->authService->authenticate($data['username'], $data['password']);
+
+            if ($response['success']) {
+                $user->setSession(['user_id' => $response['user']['user_id'], 'username' => $response['user']['username']]);
+                $test = $this->handleSuccessfulLogin($response);
+            } else {
+                $this->handleFailedLogin($response['errors']);
+            }
+        } else {
+            //echo json_encode(['success' => false, 'errors' => $errors]);
+            
+        }
+
+        include 'views/auth/login.php';
+
+
+        /*
+        
+        $login = new Login();
+        
         if (isset($_POST['username']) && isset($_POST['password'])) {
             $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
             $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
@@ -29,8 +59,8 @@ class LoginController {
                 } else {
                     // Successful login
                     $user->setSession(['user_id' => $response['user']['user_id'], 'username' => $response['user']['username']]);
-                    header('Location: /dashboard');
-                    exit;
+                    //header('Location: /dashboard');
+                    //exit;
                 }
             } catch (Exception $e) {
                 // Handle exceptions that are critical errors
@@ -38,9 +68,54 @@ class LoginController {
                 // Log the detailed error message for debugging
                 error_log($e->getMessage());
             }
-        }
-
-        include 'views/auth/login.php';
+        }*/
         
     }
+
+    private function getRequestData() {
+        if ($_SERVER['CONTENT_TYPE'] === 'application/json') {
+            $inputJSON = file_get_contents('php://input');
+            echo json_decode($inputJSON, true);
+        } else {
+            return [
+                'username' => filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING),
+                'password' => filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING)
+            ];
+        }
+    }
+
+    private function handleSuccessfulLogin($user) {
+        // Set session variables
+        //$_SESSION['user_id'] = $user;
+        //$_SESSION['username'] = $user['username'];
+    
+        if ($this->isAjaxRequest()) {
+            // Return JSON response for AJAX request
+            echo json_encode(['success' => true, 'redirect' => '/dashboard']);
+        } else {
+            // Redirect to the dashboard for normal requests
+            header('Location: /dashboard');
+        }
+        exit;
+    }
+    
+    private function isAjaxRequest() {
+        return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+    }
+
+    private function handleFailedLogin($errors) {
+        if ($this->isAjaxRequest()) {
+            // Return JSON response for AJAX request
+            echo json_encode(['success' => false, 'errors' => $errors]);
+        } else {
+            // Set session errors for normal requests
+            $_SESSION['login_errors'] = $errors;
+            header('Location: /login'); // Redirect back to the login page
+        }
+        exit;
+    }
+
+
 }
+
+?>
--- controllers/RegisterController.php
+++ controllers/RegisterController.php
@@ -6,18 +6,46 @@ require_once __DIR__ . '/../models/User.php';
 require_once __DIR__ . '/../models/Register.php';
 
 class RegisterController {
+    
+    private $registerService;
+
+    public function __construct() {
+        $this->registerService = new Registration();
+        $this->user = new User();
+    }
+
+
     public function index() {
 
         $user = new User();
-
+        
         // Check if user is logged in
         if ($user->isLoggedIn()) {
             header('Location: /dashboard');
             exit;
         }
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            
+            $data = $this->getRequestData();
+
+            $username = $data['username'];
+            $password = $data['password'];
+            $confirmPassword = $data['confirmPassword'];
+            $email = $data['email'];
+            $firstName = $data['firstName'];
+            $lastName = $data['lastName'];
+
+            $response = $this->registerService->register($username, $password, $confirmPassword, $email, $firstName, $lastName);
+
+            if ($response['success']) {
+                $this->handleSuccessfulRegistration($response);
+            } else {
+                $this->handleFailedRegistration($response['errors']);
+            }
+
+            /**$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
             $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
             $confirmPassword = filter_input(INPUT_POST, 'confirmPassword', FILTER_SANITIZE_STRING);
             $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
@@ -43,11 +71,58 @@ class RegisterController {
                 // Handle unexpected errors
                 $_SESSION['registration_errors'] = ['An unexpected error occurred. Please try again later.' . $e->getMessage()];
 
-            }
+            }**/
+        } else {
+            //echo json_encode(['success' => false, 'errors' => $errors]);
         }
         
         include 'views/auth/register.php';
 
+    }
+
+    private function getRequestData() {
+        if ($_SERVER['CONTENT_TYPE'] === 'application/json') {
+            $json = file_get_contents('php://input');
+            return json_decode($json, true);
+        } else {
+            return [
+                'username' => filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING),
+                'password' => filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING),
+                'confirmPassword' => filter_input(INPUT_POST, 'confirmPassword', FILTER_SANITIZE_STRING),
+                'email' => filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING),
+                'firstName' => filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_STRING),
+                'lastName' => filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_STRING)
+            ];
+        }
+    }
+
+    private function isAjaxRequest() {
+        return !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
+    }
+
+    private function handleFailedRegistration($errors) {
+        if ($this->isAjaxRequest()) {
+            echo json_encode(['success' => false, 'errors' => $errors]);
+            exit;
+        } else {
+            $_SESSION['registration_errors'] = $errors;
+            header('Location: /register');
+        }
+    }
+
+    private function handleSuccessfulRegistration($user) {
+        // Set session variables
+        //$_SESSION['user_id'] = $user;
+        //$_SESSION['username'] = $user['username'];
+    
+        if ($this->isAjaxRequest()) {
+            // Return JSON response for AJAX request
+            echo json_encode(['success' => true, 'redirect' => '/login']);
+        } else {
+            // Redirect to the logind for normal requests
+            header('Location: /login');
+        }
+        exit;
     }
 
     public function success() {
